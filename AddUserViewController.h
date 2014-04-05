54919160278d77eabbf1028dd67cdfb73a730dda
montanaj <clairejencks@gmail.com>
2014-04-05 17:49:27 -0500

    with swipe

---
+++ AddUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddUserViewController.h
+//  PreekABooToYou
+//
+//  Created by Claire Jencks on 4/5/14.
+//  Copyright (c) 2014 Claire Jencks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddUserViewController : UIViewController
+
+@end
--- PreekABooToYou/DetailProfileViewController.m
+++ PreekABooToYou/DetailProfileViewController.m
@@ -12,18 +12,24 @@
 @interface DetailProfileViewController ()
 
 @property (strong, nonatomic) IBOutlet UILabel *phoneNumberLabel;
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *myEmail;
+@property (strong, nonatomic) IBOutlet UILabel *myAddressLabel;
+
 
 @property (strong, nonatomic) IBOutlet UITapGestureRecognizer *myTapGesture;
 @property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *myRightSwipeGesture;
 @property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *myLeftSwipeGesture;
 @property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *myUpSwipeGesture;
 @property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *myDownSwipeGesture;
-@property (strong, nonatomic) IBOutlet UIImageView *myImageView;
-@property (strong, nonatomic) IBOutlet UIView *myOverlayView;
 
-@property (strong, nonatomic) IBOutlet UILabel *nameLabel;
-@property (strong, nonatomic) IBOutlet UILabel *myEmail;
-@property (strong, nonatomic) IBOutlet UILabel *myAddressLabel;
+@property (strong, nonatomic) IBOutlet UIImageView *myImageView; //the image the originally pops up (its on the originview)
+@property (strong, nonatomic) IBOutlet UIView *myOverlayView;//the transparent one that you swipe up
+@property (strong, nonatomic) IBOutlet UIView *myMaskView; //masks the animation transition (covers the views below it) makes it look pretty
+@property (strong, nonatomic) IBOutlet UIView *myOriginView;//helps with transition, so we call self.myoriginview
+@property (strong, nonatomic) IBOutlet UIView *flipView; //the flipside view (editing)
+
+@property BOOL flipViewBOOL;
 @property (nonatomic) User *currentUser;
 
 
@@ -85,16 +91,48 @@
 
 -(IBAction)handleTap:(UITapGestureRecognizer *)tap
 {
-    
+    if (self.flipViewBOOL)
+    {
+        [UIView animateWithDuration:1.0f animations:^{
+            [UIView transitionFromView:self.flipView
+                                toView:self.myOriginView
+                              duration:0.5f
+                               options:UIViewAnimationOptionTransitionFlipFromLeft
+                            completion:nil];
+        }
+                         completion:^(BOOL finished)
+         {
+             [self.view bringSubviewToFront:self.view];
+             //             self.myRightSwipeGestureRecognizer.enabled = YES;
+             //             self.myLeftSwipeGestureRecognizer.enabled = YES;
+             //             self.myUpSwipeGestureRecognizer.enabled = YES;
+             //             self.myDownSwipeGestureRecognizer.enabled = YES;
+         }];
+    }
+    else
+    {
+        [UIView animateWithDuration:1.0f animations:^{
+            [UIView transitionFromView:self.myOriginView
+                                toView:self.flipView
+                              duration:0.5f
+                               options:UIViewAnimationOptionTransitionFlipFromRight
+                            completion:nil];
+        }
+                         completion:^(BOOL finished)
+         {
+             
+         }];
+    }
+    self.flipViewBOOL = !self.flipViewBOOL;
 }
-    
+
 
 #pragma mark -- helper methods
-//if you click on the original cell, it still segues to the next screen, but unless you call display user up above, no photo displays on the new cell. 
+//if you click on the original cell, it still segues to the next screen, but unless you call display user up above, no photo displays on the new cell.
 -(void)displayUser
 {
-    self.currentUser = self.allUsersArray[self.currentIndex];
-    self.myImageView.image = [UIImage imageWithData:self.currentUser.photo];
+        self.currentUser = self.allUsersArray[self.currentIndex];
+        self.myImageView.image = [UIImage imageWithData:self.currentUser.photo];
 }
 
 @end
